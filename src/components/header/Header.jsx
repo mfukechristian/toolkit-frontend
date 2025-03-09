dbf91c119437b3f0dbc348a5c7e6dd4cc810cc44
mfukechristian <mfukechristian@gmail.com>
2025-03-09 12:02:29 +0200

    add the header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,20 @@
 import "./Header.css";
+import logo from "../../assets/logo.webp";
 
 const Header = () => {
-  return <header className="header">hello</header>;
+  return (
+    <header className="header">
+      <div className="logo-img">
+        <img src={logo} alt="logo" />
+      </div>
+      <div className="cta">
+        <a href="https://christianmfuke.netlify.app/" target="_blank">
+          👋
+          <p>Say Hi to the Author!</p>
+        </a>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
